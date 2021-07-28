488b5eb795e941b308929f5e7652341c018e9638
waelhawa <wael.hawa86@gmail.com>
2021-07-28 09:17:15 -0400

    First Commit

---
+++ MovieLabAPI/Controllers/MovieLabController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieLabAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLabAPI.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class MovieLabController : Controller
+    {
+        private readonly MovieLabContext _context = new MovieLabContext();
+
+        public MovieLabController(MovieLabContext context)
+        {
+            _context = context;
+        }
+
+        #region Create
+        [HttpPost]
+        public async Task<ActionResult<Movie>> AddMovie(Movie newMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Movies.AddAsync(newMovie);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetMovie), new { id = newMovie.Id }, newMovie);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+
+        #region Read
+        // /api/MovieLab
+        [HttpGet]
+        public async Task<ActionResult<List<Movie>>> GetMovies()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return movie;
+            }
+        }
+
+        #endregion
+
+
+        #region Update
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateMovie(int id, Movie updatedMovie)
+        {
+            if (!ModelState.IsValid || id != updatedMovie.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var dbMovie = _context.Movies.Find(id);
+                dbMovie.Title = updatedMovie.Title;
+                dbMovie.Genre = updatedMovie.Genre;
+                dbMovie.Runtime = updatedMovie.Runtime;
+                
+
+                _context.Entry(dbMovie).State = EntityState.Modified;
+                _context.Update(dbMovie);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+        }
+
+        #endregion
+
+
+        #region Delete
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+                return NoContent();
+                //204 HTTP status code -- successful and the API is nto returning any content
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ MovieLabAPI/Models/MovieLabContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MovieLabAPI.Models
+{
+    public partial class MovieLabContext : DbContext
+    {
+        public MovieLabContext()
+        {
+        }
+
+        public MovieLabContext(DbContextOptions<MovieLabContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=MovieLab;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie");
+
+                entity.Property(e => e.Genre).HasMaxLength(20);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
